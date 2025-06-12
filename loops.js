3df2a401315fefd35855eb2503c138b7402d5498
thimna.sa <thimnasantos@gmail.com>
2025-06-12 18:50:30 -0300

    loops

---
+++ loops.js
@@ -0,0 +1,25 @@
+let list = [10, "teste", 85 , "palavra", 89 , "QA"]
+let x = 0
+
+console.log(list)
+console.log(list.length)
+
+//while (x < list.length) {
+     //console.log("numero de X é : " + x)
+    //console.log("------------------------------------------")
+    //console.log(" Valor da posicao é: " + list[x])
+    //console.log("-----------------------------------")
+    
+
+  //  x = x + 1
+//}
+
+let listFor = ["teste2", 26, "QA", 99, 80]
+
+for (let index = 0; index < listFor.length; index++) {
+    console.log("numero de index é : " + index)
+    console.log("------------------------------------------")
+    console.log(" Valor da posicao é: " + list[index])
+    console.log("-----------------------------------")
+    
+}
