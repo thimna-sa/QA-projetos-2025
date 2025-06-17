1053e618deffc58a4af292fed0e4b333341ae601
thimna.sa <thimnasantos@gmail.com>
2025-06-17 17:16:22 -0300

    commit2

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,27 @@
+describe('Orange HRM Tests', () => {
+  it('login com sucess', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Admin')
+    cy.get("[name='password']").type('admin123')
+    cy.get("[type='submit']").click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+  
+  
+  
+  
+  })
+ it('login - Fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Admin')
+    cy.get("[name='password']").type('admin')
+    cy.get("[type='submit']").click()
+    cy.get('.oxd-alert-content')
+   
+  
+  
+  
+  
+  })
+  
+})
